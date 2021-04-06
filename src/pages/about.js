cad17f3816360cb106c16f3b9282e914a5a9abaf
Richard <richard.yang.tong@gmail.com>
2021-04-06 18:31:24 -0400

    Added some placeholder contents.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-export default function About() {
+export default function about() {
     return (
         <div>
-            
+            <h1>About Page</h1>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
         </div>
     )
 }
